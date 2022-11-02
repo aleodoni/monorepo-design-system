4a0cc1c333b80ebf89c665e2ee5d809911c48555
Alexandre Odoni <aleodoni@gmail.com>
2022-11-02 15:40:13 -0300

    storybook and turbo repo config

--- packages/react/src/index.ts
+++
@@ -1,3 +0,0 @@
-import { colors } from '@cmc-ui/tokens'
-
-console.log(colors)
--- packages/tokens/dist/index.js
+++ packages/tokens/dist/index.js
@@ -23,7 +23,10 @@ __export(src_exports, {
   colors: () => colors,
   fontFamily: () => fontFamily,
   fontSizes: () => fontSizes,
-  fontWeights: () => fontWeights
+  fontWeights: () => fontWeights,
+  lineHeights: () => lineHeights,
+  radii: () => radii,
+  space: () => space
 });
 module.exports = __toCommonJS(src_exports);
 
@@ -49,6 +52,35 @@ var colors = {
   green400: "#4ade80"
 };
 
+// src/space.ts
+var space = {
+  1: "0.25rem",
+  2: "0.5rem",
+  3: "0.75rem",
+  4: "1rem",
+  5: "1.25rem",
+  6: "1.5rem",
+  7: "1.75rem",
+  8: "2rem",
+  10: "2.5rem",
+  12: "3rem",
+  16: "4rem",
+  20: "5rem",
+  40: "10rem",
+  64: "16rem",
+  80: "20rem"
+};
+
+// src/radii.ts
+var radii = {
+  px: "1px",
+  xs: "4px",
+  sm: "6px",
+  md: "8px",
+  lg: "16px",
+  full: "99999px"
+};
+
 // src/font-family.ts
 var fontFamily = {
   default: "Roboto, sans-serif",
@@ -57,12 +89,19 @@ var fontFamily = {
 
 // src/font-sizes.ts
 var fontSizes = {
-  xs: 14,
-  sm: 16,
-  md: 18,
-  lg: 20,
-  xl: 24,
-  "2xl": 32
+  xxs: "0.625rem",
+  xs: "0.75rem",
+  sm: "0.875rem",
+  md: "1rem",
+  lg: "1.125rem",
+  xl: "1.25rem",
+  "2xl": "1.5rem",
+  "4xl": "2rem",
+  "5xl": "2.25rem",
+  "6xl": "3rem",
+  "7xl": "4rem",
+  "8xl": "4.5rem",
+  "9xl": "6rem"
 };
 
 // src/font-weights.ts
@@ -71,10 +110,21 @@ var fontWeights = {
   medium: "500",
   bold: "700"
 };
+
+// src/line-heights.ts
+var lineHeights = {
+  shorter: "125%",
+  short: "140%",
+  base: "160%",
+  tall: "180%"
+};
 // Annotate the CommonJS export names for ESM import in node:
 0 && (module.exports = {
   colors,
   fontFamily,
   fontSizes,
-  fontWeights
+  fontWeights,
+  lineHeights,
+  radii,
+  space
 });
